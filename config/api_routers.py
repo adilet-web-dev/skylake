1c10e02619ae21803726fe25b157109e645cc723
JKLeorio <jkdplatinum@gmail.com>
2022-02-26 03:32:07 +0600

    new urls for search

--- config/api_routers.py
+++ config/api_routers.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
+from django.urls import path
+from debates.api.views import DebateViewSet , search_with_candidate_view,search_debate_view
 
-from debates.api.views import DebateViewSet
 
 
 if settings.DEBUG:
@@ -12,5 +13,10 @@ else:
 router.register("debates", DebateViewSet)
 
 app_name = "api"
-urlpatterns = router.urls
+
+urlpatterns=[
+    path("search/<str:topic>", search_debate_view.as_view()),
+    path("search_c/<str:candidate>", search_with_candidate_view.as_view()),
+]
+urlpatterns+=router.urls
 
