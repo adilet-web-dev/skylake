3db3413d92e6871282386a199651c2d83da0f8fc
mrAdiletDev <adiletdj19@gmail.com>
2022-02-27 21:12:47 +0600

    add allowed host and fix DEBUG constant

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG_MODE")
+DEBUG = os.environ.get("DEBUG_MODE") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['skylake-debates.herokuapp.com']
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
